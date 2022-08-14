39ef902f17a97fcb03f89b8bb6d1c441b71cdae0
Amit Tiwari <tiwamit44@gmail.com>
2022-08-14 11:54:23 +0530

    Update Employee Repository
    Adding changes to Employee Repository for Delete Operations.

--- src/main/java/com/example/amit/EmployeeRepository.java
+++ src/main/java/com/example/amit/EmployeeRepository.java
@@ -7,5 +7,11 @@ import org.springframework.stereotype.Repository;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 	Employee findByEmployeeId(int id);
+	
+	
+	@Modifying // Need to use this for DML operations
+	@Query("delete from Employee where employeeId=:id")
+	void deleteByEmployeeId(@Param(value = "id") int id);
+	
 
 }
